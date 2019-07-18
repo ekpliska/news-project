b84596bc02d4b875ca396b4e8ef466f81737254a
ekpliska <pliska.ek@gmail.com>
2019-07-18 12:53:35 +0300

    created: fetch response

--- src/App.js
+++ src/App.js
@@ -3,37 +3,28 @@ import './App.css';
 import News from './components/News';
 import Add from './components/Add';
 
-const myNews = [
-	{
-		id: 1,
-		author: "Саша Печкин",
-		text: "В четверг, четвертого числа...",
-		bigText: "в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж."
-	},
-	{
-		id: 2,
-		author: "Просто Вася",
-		text: "Считаю. что $ должен стоить 35 рублей.",
-		bigText: "А евро 42!"
-	},
-	{
-		id: 3,
-		author: "Max Frontend",
-		text: "Прошло два года, а доллар так и не стоит 35 рублей...",
-		bigText: "А евро опять выше 70."
-	},
-	{
-		id: 4,
-		author: "Гость",
-		text: "Бесплатно. Без смс и регистрации",
-		bigText: "Вся инфа на сайте, не реклама!"
-	},
-];
-
 class App extends React.Component {
 
 	state = {
-		news: myNews
+		news: null,
+		isLoading: false
+	}
+
+	componentDidMount() {
+		this.setState({
+			isLoading: true
+		});
+
+		fetch('http://localhost:3000/data/newsData.json')
+		.then(response => {
+			return response.json()
+		})
+		.then(data => {
+			this.setState({
+				news: data,
+				isLoading: false
+			})
+		});
 	}
 
 	handleAddNews = (data) => {
@@ -46,11 +37,13 @@ class App extends React.Component {
 	}
 
 	render() {
+		const { news, isLoading } = this.state;
 		return (
 			<div>
 				<Add onAddNews={this.handleAddNews} />
 				<h3>Новости</h3>
-				<News data={this.state.news} />
+				{isLoading && <p>Загружаю...</p>}
+				{Array.isArray(news) && <News data={news} />}
 			</div>
 		)
 	}  
